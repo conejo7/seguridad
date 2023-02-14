e2c8271dff183ee377fe4d779590bc058ab91e0e
conejo7 <erick_s5490@hotmail.com>
2023-02-13 23:01:23 -0500

    primer commit

---
+++ src/main/java/com/escandalosos/springbootbackendtoken/SpringBootBackendTokenApplication.java
@@ -0,0 +1,13 @@
+package com.escandalosos.springbootbackendtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escandalosos/springbootbackendtoken/SpringBootBackendTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escandalosos.springbootbackendtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
